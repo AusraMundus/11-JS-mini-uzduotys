105ac48413183be512d2252c61573286ade4ed46
Ausra <munduswebstudio@gmail.com>
2023-04-10 23:04:50 +0300

    Function tasks

--- js/02-funkcijos.js
+++ js/02-funkcijos.js
@@ -73,18 +73,128 @@ console.log(daugyba(skaicius1, skaicius3));
 
 console.log('--3.--')
 
-let a = 0;
-let b = 0;
+let c = 0;
+let d = 0;
 
-function skaitmenuKiekisSkaiciuje (a) {
-    if (typeof a !== 'number') {
-        console.log('Pateikta netinkamo tipo reikšmė.');
-    } else {
-        if (typeof a === 0) {
-        b = a.length;
-    }
-        return b;
-    }
+function skaitmenuKiekisSkaiciuje (c) {
+    if (typeof c === 'number' && isFinite(c)) {
+        d = ('' + c).length; // Skaiciu paverciam stringu
+        return d;
+        } else {
+        return `Pateikta netinkamo tipo reikšmė.`;
+        }
 }
 
 console.log(skaitmenuKiekisSkaiciuje(5));
+console.log(skaitmenuKiekisSkaiciuje(781));
+console.log(skaitmenuKiekisSkaiciuje(37060123456));
+console.log(skaitmenuKiekisSkaiciuje(true));
+console.log(skaitmenuKiekisSkaiciuje('asd'));
+console.log(skaitmenuKiekisSkaiciuje(NaN));
+
+// Kitas būdas
+
+function skaitmenuKiekisSkaiciuje (number) {
+    if (typeof number === 'number' && isFinite(number)) {
+    const numLength = ('' + number).length;
+    return numLength;
+       
+    } else {
+    return 'Pateikta netinkamo tipo reikšmė.';
+        }
+}
+console.log(skaitmenuKiekisSkaiciuje(5));
+console.log(skaitmenuKiekisSkaiciuje(781));
+console.log(skaitmenuKiekisSkaiciuje(37060123456));
+console.log(skaitmenuKiekisSkaiciuje(true));
+console.log(skaitmenuKiekisSkaiciuje('asd'));
+console.log(skaitmenuKiekisSkaiciuje(NaN));
+
+/*
+4. Funkcija pavadinimu “didziausiasSkaiciusSarase”:
+    a. priima vieną kintamąjį
+    b. jei perduotas kintamasis nėra sąrašo tipo, tai išveda pranešimą “Pateikta
+    netinkamo tipo reikšmė.”
+    c. jei sąrašas yra tuščias, tai išveda pranešimą “Pateiktas sąrašas negali būti
+    tuščias.”
+    d. priešingu atveju, funkcija tęsia darbą
+    e. pereina per visą pateiktą sąrašą ir į atskirą kintamąjį įsimena skaičių, kuris tuo
+    metu yra didžiausias
+    f. gražina didžiausią surastą skaičių
+    g. TESTAI:
+        i. console.log( didziausiasSkaiciusSarase( [ 1 ] ) );
+            1. rezultatas: 1
+        ii. console.log( didziausiasSkaiciusSarase( [ 1, 2, 3 ] ) );
+            1. rezultatas: 3
+        iii. console.log( didziausiasSkaiciusSarase( [ -5, 78, 14, 0, 18 ] ) );
+            1. rezultatas: 78
+        iv. console.log( didziausiasSkaiciusSarase( [ 69, 69, 69, 69, 66 ] ) );
+            1. rezultatas: 69
+        v. console.log( didziausiasSkaiciusSarase( [ -1, -2, -3, -4, -5, -6, -7, -8 ] ) );
+            1. rezultatas: -1
+        vi. console.log( didziausiasSkaiciusSarase( “pomidoras” ) );
+            1. rezultatas: “Pateikta netinkamo tipo reikšmė.”
+        vii. console.log( didziausiasSkaiciusSarase( [] ) );
+            1. rezultatas: “Pateiktas sąrašas negali būti tuščias.”
+*/
+
+console.log('--4.--')
+
+function didziausiasSkaiciusSarase (sarasas) {
+    if (Array.isArray(sarasas) && sarasas.length !== 0) { // kintamasis saraso tipo ir sarasas nera tuscias
+        const max = Math.max(...sarasas); // atrenka didžiausią skaičių
+        return max;
+    } else if (sarasas.length === 0) { // sarasas yra tuscias, netinka -> sarasas = []
+            return 'Pateiktas sąrašas negali būti tuščias.';
+        } else {
+            return 'Pateikta netinkamo tipo reikšmė.';
+        }
+    }
+
+console.log(didziausiasSkaiciusSarase([1]));
+console.log(didziausiasSkaiciusSarase([1, 2, 3]));
+console.log(didziausiasSkaiciusSarase([-5, 78, 14, 0, 18]));
+console.log(didziausiasSkaiciusSarase([69, 69, 69, 69, 66]));
+console.log(didziausiasSkaiciusSarase([-1, -2, -3, -4, -5, -6, -7, -8]));
+console.log(didziausiasSkaiciusSarase('pomidoras'));
+console.log(didziausiasSkaiciusSarase([]));
+
+/*
+5. Funkcija pavadinimu “isrinktiRaides”:
+    a. priima du kintamuosius:
+        i. pirmasis nurodo tekstą, su kuriuo reikės atlikti užduotį
+        ii. antrasis nurodo kas kelintą raidę išrinkti
+    b. patikrinti, ar pirmasis kintamasis yra teksto tipo:
+    i. jei ne, išvedame pranešimą “Pirmasis kintamasis yra netinkamo tipo.”
+    ii. priešingu atveju tęsiame darbą
+    c. patikrinti, ar pirmasis kintamasis yra ne tuščias tekstas ir ne didesnis nei 100
+    simbolių:
+    i. jei ne, išvedame pranešimą “Pirmojo kintamojo reikšmė yra netinkamo
+    dydžio.”
+    ii. priešingu atveju tęsiame darbą
+    d. patikrinti, ar antrasis kintamasis yra skaičiaus tipo:
+    i. jei ne, išvedame pranešimą “Antrasis kintamasis yra netinkamo tipo.”
+    ii. priešingu atveju tęsiame darbą
+    e. patikriname, ar antrojo kintamojo vertė yra didesnė už nulį:
+    i. jei ne, išvedame pranešimą “Antrasis kintamasis turi būti didesnis už nulį.”
+    ii. priešingu atveju tęsiame darbą
+    f. patikriname, ar antrojo kintamojo vertė yra ne didesnė už pirmojo kintamojo ilgį:
+    i. jei ne, išvedame pranešimą “Antrasis kintamasis turi būti ne didesnis už
+    pateikto teksto ilgį.”
+    ii. priešingu atveju tęsiame darbą
+    g. išrenkame iš nurodyto teksto kas kelintą raidę (pagal antrojo kintamojo žingsnį)
+    h. išrinktas raides sudėti į atskirą kintamąjį, kuris yra teksto tipo
+    i. gražina rezultatą
+    j. TESTAI:
+    i. console.log( isrinktiRaides( “abcdefg”, 2 ) );
+    1. rezultatas: “bdf”
+    ii. console.log( isrinktiRaides( “abcdefghijkl”, 3 ) );
+    1. rezultatas: “cfil”
+    iii. console.log( isrinktiRaides( “abc”, 0 ) );
+    1. rezultatas: “Antrasis kintamasis turi būti didesnis už nulį.”
+    iv. console.log( isrinktiRaides( “abc”, 4 ) );
+    1. rezultatas: “Antrasis kintamasis turi būti ne didesnis už pateikto
+    teksto ilgį.”
+    v. console.log( isrinktiRaides( 1561, 2 ) );
+    1. rezultatas: “Pirmasis kintamasis yra netinkamo tipo.”
+*/
--- js/codewars.js
+++ js/codewars.js
@@ -5,6 +5,7 @@ function greet () {
 }
 console.log(greet());
 
+console.log('--------------------------------')
 
 /*
 Write function bmi that calculates body mass index (bmi = weight / height2).
@@ -19,20 +20,15 @@ function bmi (weight, height) {
     const bmi = weight / (height * height);
     if (bmi <= 18.5) {
         return `Underweight`;
-    } else {
-        if (bmi <= 25) {
+    } else if (bmi <= 25) {
             return `Normal`;
-        } else {
-            if (bmi <= 30) {
+        } else if (bmi <= 30) {
                 return `Overweight`;
-            } else {
-                if (bmi > 30) {
+            } else if (bmi > 30) {
                     return `Obese`;
-                }
-            }
-        }
     }
 }
+
 console.log(bmi(80, 1.80));
 
-
+console.log('--------------------------------')
